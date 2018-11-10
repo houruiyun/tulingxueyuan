016dfa54e96333e62494e096c5c448a0967acb58
houruiyun <674211605@qq.com>
2018-11-10 12:08:40 +0800

    基础

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Create your views here.视图
+'''
+视图函数需要一个参数，类型应该是HttpRequest
+'''
+
+def do_normalmap():
+    return HttpResponse('this is noamalmap')
+
